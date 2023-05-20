df966cf1e10b5f87bf81310cffa59235ca15d388
Joshep-c <121264024+Joshep-c@users.noreply.github.com>
2023-05-19 20:25:51 -0500

    Update BienvenidoAlCurso.java

--- Vim - Git - GitHub Lab01/BienvenidoAlCurso.java
+++ Vim - Git - GitHub Lab01/BienvenidoAlCurso.java
@@ -1,12 +1,5 @@
-import java.util.Scanner;
 public class BienvenidoAlCurso{
   public static void (String args []){
-    Scanner scan = new Scanner(System.in);
-    String name;
-    
-    System.out.println("Ingrese su nombre:");
-    name = scan.nextLine();
-    
-    System.out.println("Bienvenido " + name);
+    System.out.println("Bienvenido al curso");
   }
 }
