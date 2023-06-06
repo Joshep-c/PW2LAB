b1e09cae2fc18d985935fb17adf41bf460334f73
Joshep-c <jccahuanala@unsa.edu.pe>
2023-06-05 21:42:28 -0500

    Agregando dos bucles while, el primero para los 7 diferentes ejercicios, y otro para el cierra por medio de QUIT o presionar la tecla enter

--- Python/principal.py
+++ Python/principal.py
@@ -2,27 +2,43 @@ import pygame
 from colors import *
 from picture import *
 from pieces import *
+from pygame import *
 
-WIDTH, HEIGHT = 800, 800
-BACKGROUND_COLOR = color[' ']
-PIECE_COLOR = color['_']
+def draw_tower(w, h):
+    WIDTH, HEIGHT = w, h
+    window = pygame.display.set_mode((WIDTH, HEIGHT))
+    pygame.display.set_caption("Ajedrez")
+    
 
-pygame.init()
-window = pygame.display.set_mode((WIDTH, HEIGHT))
-pygame.display.set_caption("Ajedrez")
+apertura = True
+count = 0
 
-running = True
+while apertura:
+    pygame.init()
+    draw_tower(800, 800)
 
-while running:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            running = False
-    pygame.display.update()
+    running = True
+
+    while running:
+        for event in pygame.event.get():
+            if event.type == QUIT or (event.type == KEYDOWN and event.key == K_RETURN):
+                running = False
+        pygame.display.update()
+
+    pygame.quit()
+    count += 1
+    if count == 7:
+        apertura = False
 
 
-pygame.quit()
 
 
+
+
+    
+
+
+    
 
 
 
