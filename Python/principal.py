5007c96c714deec51c270ee1b882d3ee5d871520
Joshep-c <jccahuanala@unsa.edu.pe>
2023-06-05 20:46:19 -0500

    Agregamos los archivos 'py', un main y codigo necesario para iniciar una ventana de juego

---
+++ Python/principal.py
@@ -0,0 +1,21 @@
+import pygame
+from colors import *
+from picture import Picture
+from pieces import *
+
+WIDTH, HEIGHT = 800, 800
+BACKGROUND_COLOR = color[' ']
+PIECE_COLOR = color['_']
+
+pygame.init()
+window = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption("Ajedrez")
+
+
+
+pygame.quit()
+
+
+
+
+
