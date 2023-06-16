3354a6bd2c507e3ea4cc488657c91a4987e736c2
Joshep-c <jccahuanala@unsa.edu.pe>
2023-06-16 08:14:36 -0500

    Modificacion de models.py, admin.py, setting.py. Para el desarrollo de la primera aplicación de Django.

--- Django Lab5/django_env/Proyecto/Apps/Aplicacion1/admin.py
+++ Django Lab5/django_env/Proyecto/Apps/Aplicacion1/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
+# Modificación de importe
+from .models import Video
+
 # Register your models here.
+
+# Modelo registrado para la aplicacion1
+admin.site.register(Video)
--- Django Lab5/django_env/Proyecto/Apps/Aplicacion1/models.py
+++ Django Lab5/django_env/Proyecto/Apps/Aplicacion1/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+# importación modificar comentario
+import uuid
 
 # Create your models here.
+# Primera creación
+class Video(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    titulo = models.CharField(max_length=100)
+    url = models.URLField(max_length=200)
+    fecha_carga = models.DateField(auto_now=True)
