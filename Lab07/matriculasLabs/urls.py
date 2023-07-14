326a8b3bad7a42bd765554168fca0c66be19b33a
Joshep-c <jccahuanala@unsa.edu.pe>
2023-07-13 21:40:55 -0500

    Agregacion de la funcionalidad para el renderizado del html lista en pdf.

--- Lab07/matriculasLabs/urls.py
+++ Lab07/matriculasLabs/urls.py
@@ -3,4 +3,5 @@ from .import views
 
 urlpatterns = [
     path('',views.tabla_asignaturas,name="index"),
+    path('generar-pdf/', views.generar_pdf, name='generar_pdf'),
 ]
--- Lab07/matriculasLabs/views.py
+++ Lab07/matriculasLabs/views.py
@@ -1,6 +1,33 @@
+# pdf
+
+from django.http import HttpResponse
+from django.template.loader import get_template
+from xhtml2pdf import pisa
+from io import BytesIO
+
+# Vista para la lista
+
 from django.shortcuts import render
 from .models import Asignatura
 
 def tabla_asignaturas(request):
     asignaturas = Asignatura.objects.all()
     return render(request, 'lista.html', {'asignaturas': asignaturas})
+
+
+# función para el renderizado en pdf 
+
+def generar_pdf(request):
+    template_path = 'lista.html'
+    asignaturas = Asignatura.objects.all()
+    context = {'asignaturas': asignaturas}
+    template = get_template(template_path)
+    html = template.render(context)
+
+    result = BytesIO()
+    pdf = pisa.pisaDocument(BytesIO(html.encode("ISO-8859-1")), result)
+
+    if not pdf.err:
+        return HttpResponse(result.getvalue(), content_type='application/pdf')
+
+    return HttpResponse('Error al generar el PDF: {}'.format(pdf.err), status=500)
